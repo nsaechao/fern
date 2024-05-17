ca3627131ec3d1ae6b973993bb834de8ef06e450
Andrew Jiang <andrew@buildwithfern.com>
2024-05-17 23:35:13 +0000

    Fix

--- packages/cli/cli/src/cli.ts
+++ packages/cli/cli/src/cli.ts
@@ -889,11 +889,12 @@ function addPreviewCommand(cli: Argv<GlobalCliOptions>, cliContext: CliContext) 
         (yargs) => yargs,
         async () => {
             await previewDocsWorkspace({
-                loadProject: () => loadProjectAndRegisterWorkspacesWithContext(cliContext, {
-                    defaultToAllApiWorkspaces: true,
-                    commandLineApiWorkspace: undefined
-                })
-                cliContext,
+                loadProject: () =>
+                    loadProjectAndRegisterWorkspacesWithContext(cliContext, {
+                        defaultToAllApiWorkspaces: true,
+                        commandLineApiWorkspace: undefined
+                    }),
+                cliContext
             });
         }
     );
