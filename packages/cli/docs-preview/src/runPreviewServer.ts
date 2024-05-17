6812108bfdf23bc13a243f6ee82f5e4266bbef7d
Andrew Jiang <andrew@buildwithfern.com>
2024-05-17 03:00:02 -0400

    update runPreviewServer

--- packages/cli/docs-preview/src/runPreviewServer.ts
+++ packages/cli/docs-preview/src/runPreviewServer.ts
@@ -3,7 +3,10 @@ import { TaskContext } from "@fern-api/task-context";
 import { APIWorkspace, DocsWorkspace } from "@fern-api/workspace-loader";
 import cors from "cors";
 import express from "express";
+import { watch } from "fs";
+import http from "http";
 import path from "path";
+import { WebSocketServer, type WebSocket } from "ws";
 import { getPreviewDocsDefinition } from "./previewDocs";
 
 const PATH_TO_NEXT = "/Volumes/git/fern-platform/packages/ui/local-preview-bundle/out";
@@ -18,24 +21,71 @@ export async function runPreviewServer({
     context: TaskContext;
 }): Promise<void> {
     const app = express();
+    const httpServer = http.createServer(app);
+    const wss = new WebSocketServer({ server: httpServer });
+
+    const connections = new Set<WebSocket>();
+
+    wss.on("connection", function connection(ws) {
+        connections.add(ws);
+
+        ws.on("close", function close() {
+            connections.delete(ws);
+        });
+    });
+
     app.use(cors());
 
-    const docsDefinition = await getPreviewDocsDefinition({
+    let docsDefinition = await getPreviewDocsDefinition({
         docsWorkspace,
         apiWorkspaces,
         context
     });
-    const response: DocsV2Read.LoadDocsForUrlResponse = {
-        baseUrl: {
-            domain: "localhost:3000",
-            basePath: ""
-        },
-        definition: docsDefinition,
-        lightModeEnabled: docsDefinition.config.colorsV3?.type !== "dark"
-    };
+
+    let updates = 0;
+    const opts = { recursive: true };
+    watch(docsWorkspace.absoluteFilepath, opts, (_event, path) => {
+        const lock = ++updates;
+        context.logger.info(`File ${path} has been changed. Reloading...`);
+        getPreviewDocsDefinition({
+            docsWorkspace,
+            apiWorkspaces,
+            context
+        }).then((newDocsDefinition) => {
+            if (lock !== updates) {
+                return;
+            }
+            docsDefinition = newDocsDefinition;
+            connections.forEach((ws) => {
+                ws.send(JSON.stringify({ reload: true }));
+            });
+            context.logger.info("Docs reloaded");
+        });
+    });
 
     app.post("/v2/registry/docs/load-with-url", async (_, res) => {
+        const response: DocsV2Read.LoadDocsForUrlResponse = {
+            baseUrl: {
+                domain: "localhost:3000",
+                basePath: ""
+            },
+            definition: docsDefinition,
+            lightModeEnabled: docsDefinition.config.colorsV3?.type !== "dark"
+        };
         res.send(response);
+    });
+
+    app.get(/^\/_local\/(.*)/, (req, res) => {
+        return res.sendFile(`/${req.params[0]}`);
+    });
+
+    app.get("/", (req, _res, next) => {
+        if (req.headers.upgrade === "websocket") {
+            return wss.handleUpgrade(req, req.socket, Buffer.alloc(0), (ws) => {
+                wss.emit("connection", ws, req);
+            });
+        }
+        return next();
     });
 
     app.use("/_next", express.static(path.join(PATH_TO_NEXT, "_next")));
