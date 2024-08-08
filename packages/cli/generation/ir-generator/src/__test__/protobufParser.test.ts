f9782c5601df655f1bdb1a03fcdb1b6d8cf621f4
Alex McKinney <alexmckinney01@gmail.com>
2024-08-08 13:02:14 -0400

    Refactor with table-driven tests

--- packages/cli/generation/ir-generator/src/__test__/protobufParser.test.ts
+++ packages/cli/generation/ir-generator/src/__test__/protobufParser.test.ts
@@ -4,66 +4,63 @@ import { ProtobufFileInfo, ProtobufParser } from "../parsers/ProtobufParser";
 
 const TEST_DEFINITIONS = AbsoluteFilePath.of(path.join(__dirname, "parsers/protobuf/test-definitions"));
 
+interface TestCase {
+    filename: string;
+    expected: ProtobufFileInfo;
+}
+
 describe("protobuf parser", () => {
     const parser = new ProtobufParser();
 
-    it("empty", async () => {
-        const expected: ProtobufFileInfo = {
-            csharpNamespace: undefined,
-            packageName: undefined,
-            serviceName: undefined
-        };
-        const actual = await parser.parse({
-            absoluteFilePath: join(TEST_DEFINITIONS, RelativeFilePath.of("empty.proto"))
-        });
-        expect(actual).toEqual(expected);
-    });
+    const testCases: TestCase[] = [
+        {
+            filename: "empty.proto",
+            expected: {
+                csharpNamespace: undefined,
+                packageName: undefined,
+                serviceName: undefined
+            }
+        },
+        {
+            filename: "exhaustive.proto",
+            expected: {
+                csharpNamespace: "User.V1",
+                packageName: "user.v1",
+                serviceName: "User"
+            }
+        },
+        {
+            filename: "option.proto",
+            expected: {
+                csharpNamespace: "User.V1",
+                packageName: undefined,
+                serviceName: undefined
+            }
+        },
+        {
+            filename: "package.proto",
+            expected: {
+                csharpNamespace: undefined,
+                packageName: "user.v1",
+                serviceName: undefined
+            }
+        },
+        {
+            filename: "service.proto",
+            expected: {
+                csharpNamespace: undefined,
+                packageName: undefined,
+                serviceName: "User"
+            }
+        }
+    ];
 
-    it("exhaustive", async () => {
-        const expected: ProtobufFileInfo = {
-            csharpNamespace: "User.V1",
-            packageName: "user.v1",
-            serviceName: "User"
-        };
-        const actual = await parser.parse({
-            absoluteFilePath: join(TEST_DEFINITIONS, RelativeFilePath.of("exhaustive.proto"))
+    testCases.forEach((testCase) => {
+        it(testCase.filename, async () => {
+            const actual = await parser.parse({
+                absoluteFilePath: join(TEST_DEFINITIONS, RelativeFilePath.of(testCase.filename))
+            });
+            expect(actual).toEqual(testCase.expected);
         });
-        expect(actual).toEqual(expected);
-    });
-
-    it("option", async () => {
-        const expected: ProtobufFileInfo = {
-            csharpNamespace: "User.V1",
-            packageName: undefined,
-            serviceName: undefined
-        };
-        const actual = await parser.parse({
-            absoluteFilePath: join(TEST_DEFINITIONS, RelativeFilePath.of("option.proto"))
-        });
-        expect(actual).toEqual(expected);
-    });
-
-    it("package", async () => {
-        const expected: ProtobufFileInfo = {
-            csharpNamespace: undefined,
-            packageName: "user.v1",
-            serviceName: undefined
-        };
-        const actual = await parser.parse({
-            absoluteFilePath: join(TEST_DEFINITIONS, RelativeFilePath.of("package.proto"))
-        });
-        expect(actual).toEqual(expected);
-    });
-
-    it("service", async () => {
-        const expected: ProtobufFileInfo = {
-            csharpNamespace: undefined,
-            packageName: undefined,
-            serviceName: "User"
-        };
-        const actual = await parser.parse({
-            absoluteFilePath: join(TEST_DEFINITIONS, RelativeFilePath.of("service.proto"))
-        });
-        expect(actual).toEqual(expected);
     });
 });
