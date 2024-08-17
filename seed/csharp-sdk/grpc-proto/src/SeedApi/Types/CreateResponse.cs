349bed8c990922f1ad2f587a43d4012f746094e9
Alex McKinney <alexmckinney01@gmail.com>
2024-08-17 16:38:04 -0400

    Generate sdk types

--- seed/csharp-sdk/grpc-proto/src/SeedApi/Types/CreateResponse.cs
+++ seed/csharp-sdk/grpc-proto/src/SeedApi/Types/CreateResponse.cs
@@ -8,4 +8,14 @@ public record CreateResponse
 {
     [JsonPropertyName("user")]
     public UserModel? User { get; set; }
+
+    internal User.V1.CreateResponse ToProto()
+    {
+        var result = new User.V1.CreateResponse();
+        if (User != null)
+        {
+            result.User = User.ToProto();
+        }
+        return result;
+    }
 }
